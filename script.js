2d66efa6262de3c62a219221f85796a533fb4010
Notchripoff <nitchyhj@gmail.com>
2024-09-13 02:36:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function toggleStartMenu() {
-    const startMenu = document.getElementById('start-menu');
-    startMenu.classList.toggle('hidden');
-}
+const startButton = document.querySelector('.start-button');
 
-function toggleWindow() {
-    const windowElement = document.getElementById('window');
-    windowElement.classList.toggle('hidden');
-}
+startButton.addEventListener('click', () => {
+    // Display a basic menu
+    const menu = document.createElement('div');
+    menu.innerHTML = `
+        <ul>
+            <li>Programs</li>
+            <li>Documents</li>
+            <li>Pictures</li>
+        </ul>
+    `;
+    menu.style.position = 'absolute';
+    menu.style.top = '40px';
+    menu.style.left = '0';
+    menu.style.background = '#fff';
+    menu.style.padding = '10px';
+    menu.style.border = '1px solid #ddd';
+    document.body.appendChild(menu);
+});
